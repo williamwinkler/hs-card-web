55e3b90499bc325c802f6b7442c84cfec4f0bea8
William Winkler <williambwinkler@gmail.com>
2026-03-01 23:43:24 +0100

    chore: add allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['hscards.william-winkler.com'],
+  },
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
